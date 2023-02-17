5ec1c5b961b47e33b601855813d8a2306a66b192
FrantiskaAli <tiskarechk@gmail.com>
2023-02-17 10:51:51 +0000

    in this commit I made sure inquirer displays questions correctly and started with some basic point for markdown file that is to be created after

--- index.js
+++ index.js
@@ -5,8 +5,33 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {type:'input',
+    message: 'Please, eneter title of your application',
+    name:'title'},
+    {type: 'input',
+    message:'Describe your application',
+    name:'describtion'},
+    {type:'input',
+    message: 'How to install your application?',
+    name:'installation'},
+    {type:'input',
+    message: 'How to use your application?',
+    name:'usage'}, 
+    {type: 'list',
+    message: 'Pick your license',
+    choices: ['one','two', 'three'],
+    default: 3,
+    name:'license'},
+    {type: 'input',
+    message: 'Application Contributors',
+    name: 'contributors'},//will need to create an array of all the contributors  separetelly
+    {type: 'input',
+    message: 'Tests done on this application so far',
+    name:'tests'}
 
-];
+    //  , {'Questions'}
+]
+;
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -14,7 +39,11 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+inquirer
+.prompt(questions)
+.then((answers) => {
+  generateMarkdown(answers)
+})
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,26 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Describtion
+${data.describtion}
+## Table of content
+- [Installation](path)
+- [Usage](path)
+- [License](path)
+- [Contributing](path)
+- [Tests](path)
+- [Questions](path)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${data.license}
+## Contributing
+${data.contributors}
+## Tests
+${data.tests}
+## Questions
 
 `;
 }
